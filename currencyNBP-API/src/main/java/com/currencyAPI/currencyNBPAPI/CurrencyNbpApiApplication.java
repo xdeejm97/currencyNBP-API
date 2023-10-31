b049e2e3bfbfbf9a1dc7bd3776037ac4bcccf5a5
xdeejm97 <d.niesmialowski@gmail.com>
2023-10-31 21:58:02 +0100

    First commit with pushing gitignore

---
+++ currencyNBP-API/src/main/java/com/currencyAPI/currencyNBPAPI/CurrencyNbpApiApplication.java
@@ -0,0 +1,13 @@
+package com.currencyAPI.currencyNBPAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyNbpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyNbpApiApplication.class, args);
+	}
+
+}
---
+++ currencyNBP-API/src/test/java/com/currencyAPI/currencyNBPAPI/CurrencyNbpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currencyAPI.currencyNBPAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyNbpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
